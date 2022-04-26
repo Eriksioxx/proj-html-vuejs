976a8978b7541e811537cb4130d032bbbc6072a9
erik schievenin <erik.schievenin@gmail.com>
2022-04-26 21:22:39 +0200

    navbar complet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <HeaderBox logo="/classic_shop_logo1x.png" />
+    <HeaderBox :navbar="navbar" />
   </div>
 </template>
 
@@ -13,7 +13,41 @@ export default {
     HeaderBox,
   },
   data() {
-    return {};
+    return {
+      // HeaderBox: Navbar Menu
+      navbar: [
+        {
+          id: "1",
+          label: "Home",
+          url: "#",
+        },
+        {
+          id: "2",
+          label: "Shop",
+          url: "#",
+        },
+        {
+          id: "3",
+          label: "Products",
+          url: "#",
+        },
+        {
+          id: "4",
+          label: "Categories",
+          url: "#",
+        },
+        {
+          id: "5",
+          label: "News",
+          url: "#",
+        },
+        {
+          id: "6",
+          label: "Elements",
+          url: "#",
+        },
+      ],
+    };
   },
 };
 </script>
